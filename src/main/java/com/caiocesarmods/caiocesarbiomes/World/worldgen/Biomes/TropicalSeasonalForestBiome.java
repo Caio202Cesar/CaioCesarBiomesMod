29061e9f7d304923915982f44e9a82909ccb8afa
Caio202Cesar <caiodnobre@gmail.com>
2024-12-08 14:54:25 -0300

    Added Citrus Trees to Tropical Seasonal Forest biome.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/TropicalSeasonalForestBiome.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/TropicalSeasonalForestBiome.java
@@ -58,6 +58,7 @@ public class TropicalSeasonalForestBiome {
       ModDefaultBiomeFeatures.withTropicalForestDeciduousTrees(biomegenerationsettings$builder);
       ModDefaultBiomeFeatures.withTropicalPlants(biomegenerationsettings$builder);
       ModDefaultBiomeFeatures.withTropicalHibiscus(biomegenerationsettings$builder);
+      ModDefaultBiomeFeatures.withCitrusTrees(biomegenerationsettings$builder);
 
       biomegenerationsettings$builder.withFeature(GenerationStage.Decoration.LAKES, Features.LAKE_LAVA);
 
