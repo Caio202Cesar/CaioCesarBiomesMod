257655a0dea4b9d2a0fb2151fb53a0f6ea33a100
Caio202Cesar <caiodnobre@gmail.com>
2024-12-20 22:50:24 -0300

    Added Christmas Grove Biome and changed portal generation of Pewen Grove from Jungle to Mountain.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/ChristmasGroveBiome.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/ChristmasGroveBiome.java
@@ -1,8 +1,11 @@
 package com.caiocesarmods.caiocesarbiomes.World.worldgen.Biomes;
 
 import com.caiocesarmods.caiocesarbiomes.CaioCesarBiomesMod;
+import com.caiocesarmods.caiocesarbiomes.World.worldgen.Biomes.Util.ModConfiguredSurfaceBuilders;
 import com.caiocesarmods.caiocesarbiomes.World.worldgen.features.features.ModDefaultBiomeFeatures;
 import net.minecraft.client.audio.BackgroundMusicTracks;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.world.biome.*;
 import net.minecraft.world.gen.GenerationStage;
@@ -17,28 +20,33 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.function.Supplier;
 
-public class PewenConiferousGrove {
+import static net.minecraft.world.biome.BiomeMaker.makeTaigaBiome;
+
+public class ChristmasGroveBiome {
     public static final DeferredRegister<Biome> BIOMES
             = DeferredRegister.create(ForgeRegistries.BIOMES, CaioCesarBiomesMod.MOD_ID);
 
     private static ConfiguredSurfaceBuilder<?> DefaultSurfaceBuilder;
-    public static final RegistryObject<Biome> PEWEN_CONIFEROUS_GROVE = BIOMES.register("pewen_coniferous_grove",
-            () -> makePewenConiferousGroveBiome(() -> ConfiguredSurfaceBuilders.MOUNTAIN, 0.7f, 0.9f));
+    public static final RegistryObject<Biome> CHRISTMAS_GROVE = BIOMES.register("christmas_grove",
+            () -> makeChristmasGroveBiome(() -> ModConfiguredSurfaceBuilders.MEDITERRANEAN_SURFACE, 0.1f, 0.18f));
 
 
-    private static Biome makePewenConiferousGroveBiome(final Supplier<ConfiguredSurfaceBuilder<?>> surfaceBuilder, float depth, float scale) {
+    private static Biome makeChristmasGroveBiome(final Supplier<ConfiguredSurfaceBuilder<?>> surfaceBuilder, float depth, float scale) {
         MobSpawnInfo.Builder mobspawninfo$builder = new MobSpawnInfo.Builder();
         DefaultBiomeFeatures.withPassiveMobs(mobspawninfo$builder);
+        mobspawninfo$builder.withSpawner(EntityClassification.CREATURE, new MobSpawnInfo.Spawners(EntityType.WOLF, 8, 4, 4));
+        mobspawninfo$builder.withSpawner(EntityClassification.CREATURE, new MobSpawnInfo.Spawners(EntityType.RABBIT, 4, 2, 3));
+        mobspawninfo$builder.withSpawner(EntityClassification.CREATURE, new MobSpawnInfo.Spawners(EntityType.FOX, 8, 2, 4));
         DefaultBiomeFeatures.withBatsAndHostiles(mobspawninfo$builder);
 
         BiomeGenerationSettings.Builder biomegenerationsettings$builder =
                 (new BiomeGenerationSettings.Builder()).withSurfaceBuilder(surfaceBuilder);
 
         biomegenerationsettings$builder.withStructure(StructureFeatures.MINESHAFT);
-        biomegenerationsettings$builder.withStructure(StructureFeatures.RUINED_PORTAL_MOUNTAIN);
         biomegenerationsettings$builder.withStructure(StructureFeatures.BURIED_TREASURE);
         biomegenerationsettings$builder.withStructure(StructureFeatures.PILLAGER_OUTPOST);
-        biomegenerationsettings$builder.withStructure(StructureFeatures.JUNGLE_PYRAMID);
+        biomegenerationsettings$builder.withStructure(StructureFeatures.VILLAGE_TAIGA);
+
 
         DefaultBiomeFeatures.withCavesAndCanyons(biomegenerationsettings$builder);
         DefaultBiomeFeatures.withMonsterRoom(biomegenerationsettings$builder);
@@ -47,20 +55,20 @@ public class PewenConiferousGrove {
         DefaultBiomeFeatures.withClayDisks(biomegenerationsettings$builder);
         DefaultBiomeFeatures.withStrongholdAndMineshaft(biomegenerationsettings$builder);
         DefaultBiomeFeatures.withFossils(biomegenerationsettings$builder);
-        DefaultBiomeFeatures.withJungleGrass(biomegenerationsettings$builder);
+        DefaultBiomeFeatures.withDefaultFlowers(biomegenerationsettings$builder);
+        DefaultBiomeFeatures.withNormalMushroomGeneration(biomegenerationsettings$builder);
         DefaultBiomeFeatures.withForestRocks(biomegenerationsettings$builder);
-        DefaultBiomeFeatures.withEmeraldOre(biomegenerationsettings$builder);
         DefaultBiomeFeatures.withLargeFern(biomegenerationsettings$builder);
-        DefaultBiomeFeatures.withAllForestFlowerGeneration(biomegenerationsettings$builder);
+        DefaultBiomeFeatures.withSparseBerries(biomegenerationsettings$builder);
         ModDefaultBiomeFeatures.withMonkeyPuzzleTrees(biomegenerationsettings$builder);
 
         biomegenerationsettings$builder.withFeature(GenerationStage.Decoration.LAKES, Features.LAKE_LAVA);
         DefaultBiomeFeatures.withFrozenTopLayer(biomegenerationsettings$builder);
 
-        return (new Biome.Builder()).precipitation(Biome.RainType.RAIN).category(Biome.Category.FOREST).depth(depth).scale(scale)
-                .temperature(0.6F).downfall(0.8F).setEffects((new BiomeAmbience.Builder()).setWaterColor(993300)
-                        .setWaterFogColor(993300).withSkyColor(12966647).withFoliageColor(8293460)
-                        .withGrassColor(11437360).setFogColor(14807295)
+        return (new Biome.Builder()).precipitation(Biome.RainType.SNOW).category(Biome.Category.TAIGA).depth(depth).scale(scale)
+                .temperature(0.0F).downfall(0.8F).setEffects((new BiomeAmbience.Builder()).setWaterColor(4159204)
+                        .setWaterFogColor(329011).withSkyColor(12966647).withFoliageColor(6332795)
+                        .withGrassColor(8434839).setFogColor(6857828)
                         .setAmbientSound(SoundEvents.MUSIC_CREATIVE)
                         .setMusic(BackgroundMusicTracks.getDefaultBackgroundMusicSelector(SoundEvents.MUSIC_CREATIVE))
                         .build())
@@ -71,4 +79,3 @@ public class PewenConiferousGrove {
         BIOMES.register(eventBus);
     }
 }
-
