7f4ed2d6279507532a7633df34119e4a7a954148
Caio202Cesar <caiodnobre@gmail.com>
2025-01-12 21:38:13 -0300

    Added fruit tags

---
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/Saplings/JapanesePineSapling.java
@@ -0,0 +1,48 @@
+package com.caiocesarmods.caiocesarbiomes.block.Saplings;
+
+import com.caiocesarmods.caiocesarbiomes.World.worldgen.features.features.TreeFeatures;
+import com.caiocesarmods.caiocesarbiomes.block.ModBlocks;
+import net.minecraft.block.*;
+import net.minecraft.block.trees.Tree;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+import java.util.Random;
+
+public class JapanesePineSapling extends SaplingBlock {
+    public JapanesePineSapling() {
+        super(new JapanesePineSapling.JapanesePineTree(), AbstractBlock.Properties.from(Blocks.OAK_SAPLING).hardnessAndResistance(0.0f)
+                .sound(SoundType.PLANT));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void registerRenderLayer() {
+        RenderTypeLookup.setRenderLayer(ModBlocks.JAPANESE_PINE_SAPLING.get(), RenderType.getCutout());
+    }
+
+    public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+
+        return 80;
+    }
+
+    public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+
+        return 60;
+    }
+
+    private static class JapanesePineTree extends Tree {
+        @Nullable
+        @Override
+        protected ConfiguredFeature<BaseTreeFeatureConfig, ?> getTreeFeature(Random random, boolean p_225546_2_) {
+            return TreeFeatures.JAPANESE_PINE_TREE;
+        }
+    }
+}
