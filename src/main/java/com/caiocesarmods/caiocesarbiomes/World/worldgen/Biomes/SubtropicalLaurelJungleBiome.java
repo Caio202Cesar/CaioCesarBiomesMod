e61e262dd4061173647b6254e8456dfa295aa86d
Caio202Cesar <caiodnobre@gmail.com>
2025-02-09 09:08:38 -0300

    Changed bay laurel plant texture and removed the pines from laurel forests.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/SubtropicalLaurelJungleBiome.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/SubtropicalLaurelJungleBiome.java
@@ -68,7 +68,7 @@ public class SubtropicalLaurelJungleBiome {
       biomegenerationsettings$builder.withFeature(GenerationStage.Decoration.LAKES, Features.LAKE_LAVA);
 
       return (new Biome.Builder()).precipitation(Biome.RainType.RAIN).category(Biome.Category.FOREST).depth(depth).scale(scale)
-              .temperature(0.8F).downfall(1.0F).setEffects((new BiomeAmbience.Builder()).setWaterColor(4159204)
+              .temperature(0.85F).downfall(1.0F).setEffects((new BiomeAmbience.Builder()).setWaterColor(4159204)
                       .setWaterFogColor(4159204).withSkyColor(7907327).withFoliageColor(5216810)
                       .withGrassColor(2791997).setFogColor(14807295)
                       .setAmbientSound(SoundEvents.MUSIC_CREATIVE)
