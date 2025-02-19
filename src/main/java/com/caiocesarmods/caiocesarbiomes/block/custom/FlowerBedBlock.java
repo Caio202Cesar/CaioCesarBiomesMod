d6b6d99744216c3c7d1cdeb3a35002620d67c02b
Caio202Cesar <caiodnobre@gmail.com>
2025-02-19 01:25:03 -0300

    Added tea leaf and matcha, which can be used to make green dye.

---
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/FlowerBedBlock.java
@@ -0,0 +1,11 @@
+package com.caiocesarmods.caiocesarbiomes.block.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.FlowerPotBlock;
+
+//This flower bed is a broader flower pot block, used to plants as large as the mediterranean herbs.
+public class FlowerBedBlock extends FlowerPotBlock {
+    public FlowerBedBlock(Block block, Properties properties) {
+        super(block, properties);
+    }
+}
