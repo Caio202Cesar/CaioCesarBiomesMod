7a5b895f16e977367107a08ae95583e8cf07cf77
Caio202Cesar <caiodnobre@gmail.com>
2025-02-23 03:27:09 -0300

    Added vine decorators to fancy jungle ocotea tree.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/features/features/CreepingFigTrunkDecorator.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/features/features/CreepingFigTrunkDecorator.java
@@ -1,10 +1,14 @@
 package com.caiocesarmods.caiocesarbiomes.World.worldgen.features.features;
 
+import com.caiocesarmods.caiocesarbiomes.block.ModBlocks;
 import com.caiocesarmods.caiocesarbiomes.block.custom.CreepingFigVine;
 import com.mojang.serialization.Codec;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.world.ISeedReader;
+import net.minecraft.world.gen.IWorldGenerationReader;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.treedecorator.TreeDecorator;
 import net.minecraft.world.gen.treedecorator.TreeDecoratorType;
@@ -29,27 +33,47 @@ public class CreepingFigTrunkDecorator extends TreeDecorator {
             if (rand.nextInt(3) > 0) {
                 BlockPos blockpos = pos.west();
                 if (Feature.isAirAt(world, blockpos)) {
-                    this.func_227424_a_(world, blockpos, CreepingFigVine.EAST, placed, boundingBox);
+                    this.placeVine(world, blockpos, CreepingFigVine.EAST, placed, boundingBox);
                 }
             }
             if (rand.nextInt(3) > 0) {
                 BlockPos blockpos1 = pos.east();
                 if (Feature.isAirAt(world, blockpos1)) {
-                    this.func_227424_a_(world, blockpos1, CreepingFigVine.WEST, placed, boundingBox);
+                    this.placeVine(world, blockpos1, CreepingFigVine.WEST, placed, boundingBox);
                 }
             }
             if (rand.nextInt(3) > 0) {
                 BlockPos blockpos2 = pos.north();
                 if (Feature.isAirAt(world, blockpos2)) {
-                    this.func_227424_a_(world, blockpos2, CreepingFigVine.SOUTH, placed, boundingBox);
+                    this.placeVine(world, blockpos2, CreepingFigVine.SOUTH, placed, boundingBox);
                 }
             }
             if (rand.nextInt(3) > 0) {
                 BlockPos blockpos3 = pos.south();
                 if (Feature.isAirAt(world, blockpos3)) {
-                    this.func_227424_a_(world, blockpos3, CreepingFigVine.NORTH, placed, boundingBox);
+                    this.placeVine(world, blockpos3, CreepingFigVine.NORTH, placed, boundingBox);
                 }
             }
         });
     }
+
+    private void placeCustomVine(IWorldGenerationReader world, BlockPos pos, BlockState state, Set<BlockPos> placed, MutableBoundingBox boundingBox) {
+        // Directly set your custom vine block at the position.
+        world.setBlockState(pos, state, 19);
+        placed.add(pos.toImmutable());
+        // Optionally, update boundingBox here if needed.
+    }
+
+    private void placeVine(IWorldGenerationReader world, BlockPos pos, BooleanProperty property, Set<BlockPos> placed, MutableBoundingBox boundingBox) {
+        // Get the passion fruit vine default state and apply the directional property.
+        BlockState vineState = ModBlocks.CREEPING_FIG_VINE.get().getDefaultState().with(property, true);
+        // Use our custom helper instead of the vanilla one.
+        this.placeCustomVine(world, pos, vineState, placed, boundingBox);
+        int i = 4;
+        for (BlockPos downPos = pos.down(); Feature.isAirAt(world, downPos) && i > 0; --i) {
+            this.placeCustomVine(world, downPos, vineState, placed, boundingBox);
+            downPos = downPos.down();
+        }
+    }
 }
+
