80103e1000aff19d23c802f72df9b5a7c260e873
Caio202Cesar <caiodnobre@gmail.com>
2025-02-23 16:38:44 -0300

    Added crape myrtle trees to subtropical laurel forests.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/BambooSubtropicalLaurelJungleBiome.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/BambooSubtropicalLaurelJungleBiome.java
@@ -63,6 +63,7 @@ public class BambooSubtropicalLaurelJungleBiome {
       ModDefaultBiomeFeatures.withSubtropicalBambooVegetation(biomegenerationsettings$builder);
       ModDefaultBiomeFeatures.withSubtropicalCitrusTrees(biomegenerationsettings$builder);
       ModDefaultBiomeFeatures.withPohutukawaTrees(biomegenerationsettings$builder);
+      ModDefaultBiomeFeatures.withCrapeMyrtles(biomegenerationsettings$builder);
 
       biomegenerationsettings$builder.withFeature(GenerationStage.Decoration.LAKES, Features.LAKE_LAVA);
 
