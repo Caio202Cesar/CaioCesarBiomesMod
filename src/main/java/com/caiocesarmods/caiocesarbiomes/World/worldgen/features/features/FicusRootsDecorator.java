bcfc1e035616c937d6094ecf9c60bcc4166a89dc
Caio202Cesar <caiodnobre@gmail.com>
2025-02-26 18:41:18 -0300

    Changes in the Ficus hanging roots: tree decoration fixed.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/features/features/FicusRootsDecorator.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/features/features/FicusRootsDecorator.java
@@ -29,37 +29,12 @@ public class FicusRootsDecorator extends TreeDecorator {
 
     @Override
     public void func_225576_a_(ISeedReader world, Random rand, List<BlockPos> logPositions, List<BlockPos> leafPositions, Set<BlockPos> changedBlocks, MutableBoundingBox boundingBox) {
-        for (BlockPos leafPos : leafPositions) {
-            BlockPos below = leafPos.down();
-
-            // Ensure there’s air below before placing vines
-            if (world.isAirBlock(below) && rand.nextFloat() < 0.3F) { // 30% chance per leaf
-                placeHangingVines(world, below, rand);
-            }
-        }
-    }
-
-    public boolean isValidPosition(BlockState state, IWorldReader world, BlockPos pos) {
-        if (world.getBlockState(pos.up()).getBlock().isIn(BlockTags.LEAVES))
-            return true;
-        return false;
-    }
-
-    private void placeHangingVines(IWorldWriter world, BlockPos pos, Random rand) {
-        BlockPos below = pos.down();
-        int maxLength = 3 + rand.nextInt(4); // Vines can hang between 3-6 blocks
-        boolean firstBlock = true;
-
-        for (int i = 0; i < maxLength; i++) {
-            if (world instanceof ISeedReader && ((ISeedReader) world).isAirBlock(below)) {
-                if (firstBlock) {
-                    world.setBlockState(below, ModBlocks.FICUS_ROOTS.get().getDefaultState(), 2);
-                    firstBlock = false;
-                } else {
-                    world.setBlockState(below, ModBlocks.FICUS_ROOTS_PLANT.get().getDefaultState(), 2);
+        for (BlockPos pos : leafPositions) {
+            if (world.getBlockState(pos.down()).isAir()) {
+                if (rand.nextInt(2) == 0) {
+                    world.setBlockState(pos.down(), ModBlocks.FICUS_ROOTS.get().getDefaultState(), 3);
                 }
-                below = below.down();
-            }  // Stop placing if we hit a solid block
+            }
         }
     }
 }
