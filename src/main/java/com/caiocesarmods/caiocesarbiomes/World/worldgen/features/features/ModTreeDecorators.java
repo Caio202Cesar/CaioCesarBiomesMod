aa885e221653b6bf0b2405a3bcae384f51149fe4
Caio202Cesar <caiodnobre@gmail.com>
2025-03-04 16:47:05 -0300

    Added a new tree to Socotra Drylands biome: socotra cucumber tree.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/features/features/ModTreeDecorators.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/features/features/ModTreeDecorators.java
@@ -15,9 +15,9 @@ public class ModTreeDecorators {
             TREE_DECORATORS.register("ficus_hanging_roots",
                     () -> new TreeDecoratorType<>(FicusRootsDecorator.CODEC));
 
-    public static final RegistryObject<TreeDecoratorType<FicusRootsDecorator>> CUCUMBER_TREE_VINE =
+    public static final RegistryObject<TreeDecoratorType<CucumberTreeVinesDecorator>> CUCUMBER_TREE_VINE =
             TREE_DECORATORS.register("cucumber_tree_vine",
-                    () -> new TreeDecoratorType<>(FicusRootsDecorator.CODEC));
+                    () -> new TreeDecoratorType<>(CucumberTreeVinesDecorator.CODEC));
 
     public static final RegistryObject<TreeDecoratorType<SpanishMossLeavesDecorator>> SPANISH_MOSS_DECORATOR =
             TREE_DECORATORS.register("spanish_moss_decorator",
