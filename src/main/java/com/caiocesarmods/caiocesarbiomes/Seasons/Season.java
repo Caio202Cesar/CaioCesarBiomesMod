3d619cdb8a2b70bd2d06eb5b1972b20881cec2a8
Caio202Cesar <caiodnobre@gmail.com>
2025-03-07 00:49:21 -0300

    Added seasonal compatibility with Serene Seasons.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/Seasons/Season.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/Seasons/Season.java
@@ -11,13 +11,13 @@ public enum Season {
 
     public static String getSeason(long dayTime) {
         long days = dayTime / 24000; // Convert ticks to days
-        long cycle = days % 84; // Assume a full year is 60 days (21 per season)
+        long cycle = days % 96; // Assume a full year is 96 days (24 per season)
 
-        if (cycle < 21) {
+        if (cycle < 24) {
             return "SPRING";
-        } else if (cycle < 42) {
+        } else if (cycle < 48) {
             return "SUMMER";
-        } else if (cycle < 63) {
+        } else if (cycle < 72) {
             return "FALL";
         } else {
             return "WINTER";
