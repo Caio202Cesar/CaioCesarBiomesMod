27add33360ab8d4a7e245bf086551b283030947f
Caio202Cesar <caiodnobre@gmail.com>
2025-03-08 07:41:21 -0300

    Fixed socotra cucumber leaves and log's item texture.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/ModEventSubscriber.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/ModEventSubscriber.java
@@ -2,6 +2,8 @@ package com.caiocesarmods.caiocesarbiomes.World.worldgen;
 
 import com.caiocesarmods.caiocesarbiomes.CaioCesarBiomesMod;
 import com.caiocesarmods.caiocesarbiomes.Seasons.Season;
+import com.caiocesarmods.caiocesarbiomes.block.ModBlocks;
+import com.caiocesarmods.caiocesarbiomes.item.CompostablesRegistry;
 import com.caiocesarmods.caiocesarbiomes.item.ModItems;
 import net.minecraft.block.ComposterBlock;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -17,5 +19,12 @@ public class ModEventSubscriber {
         Season.onRenderGameOverlay(event);
     }
 
+    @SubscribeEvent
+    public static void onCommonSetup(FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            CompostablesRegistry.addCompostable(0.5f, ModItems.ARBUTUS.get());
+
+        });
+    }
 
 }
--- src/main/java/com/caiocesarmods/caiocesarbiomes/item/CompostablesRegistry.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/item/CompostablesRegistry.java
@@ -1,19 +1,10 @@
 package com.caiocesarmods.caiocesarbiomes.item;
 
-import com.caiocesarmods.caiocesarbiomes.block.ModBlocks;
 import net.minecraft.block.ComposterBlock;
-import net.minecraft.util.IItemProvider;
+import net.minecraft.item.Item;
 
 public class CompostablesRegistry {
-    public static void init() {
-        registerCompostableItem(0.6F, ModItems.ARBUTUS.get());
-
-        registerCompostableItem(0.6F, ModBlocks.BASIL.get());
-        registerCompostableItem(0.6F, ModBlocks.ANIS.get());
-
-    }
-
-    public static void registerCompostableItem(float chance, IItemProvider itemIn) {
-        ComposterBlock.CHANCES.put(itemIn.asItem(), chance);
+    public static void addCompostable(float chance, Item item) {
+        ComposterBlock.CHANCES.put(item, chance);
     }
 }
