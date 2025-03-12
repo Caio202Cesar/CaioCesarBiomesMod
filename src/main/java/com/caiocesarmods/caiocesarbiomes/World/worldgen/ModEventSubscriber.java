3c4057de5f0fe3cd3d64f0ee233667b76041ef7d
Caio202Cesar <caiodnobre@gmail.com>
2025-03-12 08:26:58 -0300

    Socotra cucumber leaves no longer drops poison sap, as it was an error while copying from socotra's desert rose leaves' loot table,

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/ModEventSubscriber.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/ModEventSubscriber.java
@@ -2,14 +2,9 @@ package com.caiocesarmods.caiocesarbiomes.World.worldgen;
 
 import com.caiocesarmods.caiocesarbiomes.CaioCesarBiomesMod;
 import com.caiocesarmods.caiocesarbiomes.Seasons.Season;
-import com.caiocesarmods.caiocesarbiomes.block.ModBlocks;
-import com.caiocesarmods.caiocesarbiomes.item.CompostablesRegistry;
-import com.caiocesarmods.caiocesarbiomes.item.ModItems;
-import net.minecraft.block.ComposterBlock;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod.EventBusSubscriber(modid = CaioCesarBiomesMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ModEventSubscriber {
@@ -19,12 +14,5 @@ public class ModEventSubscriber {
         Season.onRenderGameOverlay(event);
     }
 
-    @SubscribeEvent
-    public static void onCommonSetup(FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> {
-            CompostablesRegistry.addCompostable(0.5f, ModItems.ARBUTUS.get());
-
-        });
-    }
 
 }
