446c378074b37abe7989cd3491f32c4e91655d9d
Caio202Cesar <caiodnobre@gmail.com>
2025-04-04 20:10:15 -0300

    Coconut and date bunch won't remain forever in their trees. It is expected to drop their fruit.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/Util/ModBrewingRecipes.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/Util/ModBrewingRecipes.java
@@ -2,52 +2,43 @@ package com.caiocesarmods.caiocesarbiomes.Util;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.potion.Potion;
-import net.minecraft.potion.Potions;
+import net.minecraft.potion.PotionUtils;
 import net.minecraftforge.common.brewing.IBrewingRecipe;
 
 public class ModBrewingRecipes implements IBrewingRecipe {
-    public ModBrewingRecipes(Potion water, Item item, Potion potion) {
+    private final Potion input;
+    private final Item ingredient;
+    private final Potion output;
+
+    public ModBrewingRecipes(Potion input, Item ingredient, Potion output) {
+        this.input = input;
+        this.ingredient = ingredient;
+        this.output = output;
     }
 
-    /**
-     * Returns true is the passed ItemStack is an input for this recipe. "Input"
-     * being the item that goes in one of the three bottom slots of the brewing
-     * stand (e.g: water bottle)
-     *
-     * @param input
-     */
     @Override
-    public boolean isInput(ItemStack input) {
-        return false;
+    public boolean isInput(ItemStack inputStack) {
+        return inputStack.getItem() == Items.POTION && PotionUtils.getPotionFromItem(inputStack) == this.input;
     }
 
-    /**
-     * Returns true if the passed ItemStack is an ingredient for this recipe.
-     * "Ingredient" being the item that goes in the top slot of the brewing
-     * stand (e.g: nether wart)
-     *
-     * @param ingredient
-     */
     @Override
-    public boolean isIngredient(ItemStack ingredient) {
-        return false;
+    public boolean isIngredient(ItemStack ingredientStack) {
+        return ingredientStack.getItem() == this.ingredient;
     }
 
-    /**
-     * Returns the output when the passed input is brewed with the passed
-     * ingredient. Empty if invalid input or ingredient.
-     *
-     * @param input
-     * @param ingredient
-     */
     @Override
-    public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
-        if (!getOutput().isEmpty()) {
-            return output;
+    public ItemStack getOutput(ItemStack inputStack, ItemStack ingredientStack) {
+        if (!this.isInput(inputStack) || !this.isIngredient(ingredientStack)) {
+            return ItemStack.EMPTY;
         }
-        return input;
+
+        ItemStack result = new ItemStack(inputStack.getItem());
+        CompoundNBT tag = inputStack.getTag() != null ? inputStack.getTag().copy() : new CompoundNBT();
+        result.setTag(tag);
+        PotionUtils.addPotionToItemStack(result, this.output);
+        return result;
     }
-        return ItemStack.EMPTY;
 }
