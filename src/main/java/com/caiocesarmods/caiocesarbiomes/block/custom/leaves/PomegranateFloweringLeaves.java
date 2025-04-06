42278c0ea1b020211ab98522530cdb76dc5a9f12
Caio202Cesar <caiodnobre@gmail.com>
2025-04-05 22:00:42 -0300

    Mandarin leaves now flowers in fall.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/leaves/PomegranateFloweringLeaves.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/leaves/PomegranateFloweringLeaves.java
@@ -38,7 +38,7 @@ public class PomegranateFloweringLeaves extends LeavesBlock implements IForgeShe
     public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
         String currentSeason = Season.getSeason(worldIn.getDayTime());
 
-        if ("FALL".equals(currentSeason) && nextStage != null && random.nextInt(15) == 0) {
+        if ("FALL".equals(currentSeason) && nextStage != null && random.nextInt(56) == 0) {
 
             int distance = state.get(LeavesBlock.DISTANCE);
             boolean persistent = state.get(LeavesBlock.PERSISTENT);
--- src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/leaves/PomegranateLeaves.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/leaves/PomegranateLeaves.java
@@ -38,7 +38,7 @@ public class PomegranateLeaves extends LeavesBlock implements IForgeShearable {
     public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
         String currentSeason = Season.getSeason(worldIn.getDayTime());
 
-        if ("SUMMER".equals(currentSeason) && nextStage != null && random.nextInt(35) == 0) {
+        if ("SUMMER".equals(currentSeason) && nextStage != null && random.nextInt(60) == 0) {
 
             int distance = state.get(LeavesBlock.DISTANCE);
             boolean persistent = state.get(LeavesBlock.PERSISTENT);
