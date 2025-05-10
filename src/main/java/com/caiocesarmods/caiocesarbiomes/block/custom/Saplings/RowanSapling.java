51b50fc6d757d17f6252cc96da6814c80ef214ed
Caio202Cesar <caiodnobre@gmail.com>
2025-05-10 18:02:12 -0300

    Changed the textures of date leaves, as well as date and canary date saplings.

---
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/Saplings/RowanSapling.java
@@ -0,0 +1,57 @@
+package com.caiocesarmods.caiocesarbiomes.block.custom.Saplings;
+
+import com.caiocesarmods.caiocesarbiomes.World.worldgen.features.features.TreeFeatures;
+import com.caiocesarmods.caiocesarbiomes.block.TreeBlocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SaplingBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.trees.Tree;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+import java.util.Random;
+
+public class RowanSapling extends SaplingBlock {
+    public RowanSapling() {
+        super(new RowanSapling.RowanTree(), Properties.from(Blocks.OAK_SAPLING).hardnessAndResistance(0.0f)
+                .sound(SoundType.PLANT));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void registerRenderLayer() {
+        RenderTypeLookup.setRenderLayer(TreeBlocks.ROWAN_SAPLING.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(TreeBlocks.POTTED_ROWAN_SAPLING.get(), RenderType.getCutout());
+
+    }
+
+    public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+
+        return 80;
+    }
+
+    public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+
+        return 60;
+    }
+
+    private static class RowanTree extends Tree {
+        @Nullable
+        @Override
+        protected ConfiguredFeature<BaseTreeFeatureConfig, ?> getTreeFeature(Random random, boolean p_225546_2_) {
+            if (random.nextInt(10) == 7) {
+                return TreeFeatures.ROWAN_TREE1;
+            } else {
+                return TreeFeatures.ROWAN_TREE2;
+            }
+        }
+    }
+}
