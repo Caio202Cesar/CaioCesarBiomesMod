caa392ddea239e59017f77e77cc73525d7d28cac
Caio202Cesar <caiodnobre@gmail.com>
2025-05-11 21:44:57 -0300

    Added a volcanic biome

--- src/main/java/com/caiocesarmods/caiocesarbiomes/block/TreeBlocks.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/TreeBlocks.java
@@ -892,11 +892,11 @@ public class TreeBlocks {
             ModLogs::new);
     public static final RegistryObject<Block> OIL_PALM_LEAVES = registerBlock("oil_palm_leaves",
             ModLeaves::new);
-    public static final RegistryObject<Block> OIL_PALM_SAPLING = registerBlock("mugo_pine_sapling",
+    public static final RegistryObject<Block> OIL_PALM_SAPLING = registerBlock("oil_palm_sapling",
             MugoPineSapling::new);
     @SuppressWarnings("deprecation")
-    public static final RegistryObject<Block> POTTED_OIL_PALM_SAPLING = BLOCKS.register("potted_mugo_pine_sapling",
-            () -> new FlowerPotBlock(TreeBlocks.MUGO_PINE_SAPLING.get(), AbstractBlock.Properties.create(Material.MISCELLANEOUS)
+    public static final RegistryObject<Block> POTTED_OIL_PALM_SAPLING = BLOCKS.register("potted_oil_palm_sapling",
+            () -> new FlowerPotBlock(TreeBlocks.OIL_PALM_SAPLING.get(), AbstractBlock.Properties.create(Material.MISCELLANEOUS)
                     .zeroHardnessAndResistance().notSolid()));
     //Oleander
     public static final RegistryObject<Block> OLEANDER_LOG = registerBlock("oleander_log",
