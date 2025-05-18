2c74c23f949ad151d4a7aa337dca7db05cdd45b4
Caio202Cesar <caiodnobre@gmail.com>
2025-05-18 16:39:35 -0300

    Ripe creeping fig is now edible.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/item/custom/MarulaItem.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/item/custom/MarulaItem.java
@@ -8,6 +8,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -29,5 +30,7 @@ public class MarulaItem extends Item {
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
         super.addInformation(stack, worldIn, tooltip, flagIn);
         tooltip.add(new StringTextComponent("Eat before it ferments and you won't get sick.").mergeStyle(TextFormatting.GRAY));
+        tooltip.add(new TranslationTextComponent("tooltip.caiocesarbiomes.marula").mergeStyle(TextFormatting.GOLD));
+
     }
 }
