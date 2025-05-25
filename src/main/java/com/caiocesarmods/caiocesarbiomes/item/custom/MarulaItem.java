8fcd7be4c6ea47721787185de7158663043bd331
Caio202Cesar <caiodnobre@gmail.com>
2025-05-25 00:16:56 -0300

    Fixed ripening problems.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/item/custom/MarulaItem.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/item/custom/MarulaItem.java
@@ -3,6 +3,7 @@ package com.caiocesarmods.caiocesarbiomes.item.custom;
 import com.caiocesarmods.caiocesarbiomes.item.ModItems;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
@@ -10,6 +11,8 @@ import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -19,10 +22,25 @@ public class MarulaItem extends Item {
         super(properties);
     }
 
-    @Override
-    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-        if (!world.isRemote && world.rand.nextFloat() < 0.01f) {
-            entity.replaceItemInInventory(slot, new ItemStack(ModItems.RIPE_MARULA.get()));
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        PlayerEntity player = event.player;
+
+        if (!player.world.isRemote && event.phase == TickEvent.Phase.END) {
+            for (int i = 0; i < player.inventory.getSizeInventory(); i++) {
+                ItemStack stack = player.inventory.getStackInSlot(i);
+
+                if (!stack.isEmpty() && stack.getItem() == ModItems.MARULA.get()) {
+                    if (player.getRNG().nextInt(100) == 70) { // ~30 in 100 chance per tick
+                        ItemStack ripeStack = new ItemStack(ModItems.RIPE_MARULA.get(), stack.getCount());
+                        if (stack.hasTag()) {
+                            ripeStack.setTag(stack.getTag().copy());
+                        }
+
+                        player.inventory.setInventorySlotContents(i, ripeStack);
+                    }
+                }
+            }
         }
     }
 
--- src/main/java/com/caiocesarmods/caiocesarbiomes/item/custom/UnripeCreepingFigItem.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/item/custom/UnripeCreepingFigItem.java
@@ -2,19 +2,37 @@ package com.caiocesarmods.caiocesarbiomes.item.custom;
 
 import com.caiocesarmods.caiocesarbiomes.item.ModItems;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class UnripeCreepingFigItem extends Item {
     public UnripeCreepingFigItem(Properties properties) {
         super(properties);
     }
 
-    @Override
-    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-        if (!world.isRemote && world.rand.nextFloat() < 0.001f) {
-            entity.replaceItemInInventory(slot, new ItemStack(ModItems.RIPE_CREEPING_FIG.get()));
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        PlayerEntity player = event.player;
+
+        if (!player.world.isRemote && event.phase == TickEvent.Phase.END) {
+            for (int i = 0; i < player.inventory.getSizeInventory(); i++) {
+                ItemStack stack = player.inventory.getStackInSlot(i);
+
+                if (!stack.isEmpty() && stack.getItem() == ModItems.UNRIPE_CREEPING_FIG.get()) {
+                    if (player.getRNG().nextInt(100) == 30) { // ~30 in 100 chance per tick
+                        ItemStack ripeStack = new ItemStack(ModItems.RIPE_CREEPING_FIG.get(), stack.getCount());
+                        if (stack.hasTag()) {
+                            ripeStack.setTag(stack.getTag().copy());
+                        }
+
+                        player.inventory.setInventorySlotContents(i, ripeStack);
+                    }
+                }
+            }
         }
     }
 }
