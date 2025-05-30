daaa50c9afe7ee7ab909da8fa8be563b24dd2598
Caio202Cesar <caiodnobre@gmail.com>
2025-05-29 22:46:42 -0300

    Added nutcracker item. It can be used to turn pecan and walnut into their edible kernel.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/item/ModItems.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/item/ModItems.java
@@ -69,7 +69,7 @@ public class ModItems {
     public static final RegistryObject<Item> PECAN_NUT = ITEMS.register("pecan_nut",
             () -> new Item(new Item.Properties().group(ModItemGroup.FRUITS_AND_VEGETABLES)));
     public static final RegistryObject<Item> PECAN_KERNEL = ITEMS.register("pecan_kernel",
-            () -> new Item(new Item.Properties().group(ModItemGroup.FRUITS_AND_VEGETABLES)));
+            () -> new Item(new Item.Properties().group(ModItemGroup.FRUITS_AND_VEGETABLES).food(ModFoods.PECAN)));
     public static final RegistryObject<Item> SWEET_CHESTNUT = ITEMS.register("sweet_chestnut",
             () -> new Item(new Item.Properties().group(ModItemGroup.FRUITS_AND_VEGETABLES)));
     public static final RegistryObject<Item> MANGO = ITEMS.register("mango",
@@ -81,7 +81,7 @@ public class ModItems {
     public static final RegistryObject<Item> WALNUT = ITEMS.register("walnut",
             () -> new Item(new Item.Properties().group(ModItemGroup.FRUITS_AND_VEGETABLES)));
     public static final RegistryObject<Item> WALNUT_KERNEL = ITEMS.register("walnut_kernel",
-            () -> new Item(new Item.Properties().group(ModItemGroup.FRUITS_AND_VEGETABLES)));
+            () -> new Item(new Item.Properties().group(ModItemGroup.FRUITS_AND_VEGETABLES).food(ModFoods.WALNUT)));
     public static final RegistryObject<Item> BREADFRUIT = ITEMS.register("breadfruit",
             () -> new Item(new Item.Properties().group(ModItemGroup.FRUITS_AND_VEGETABLES)));
     public static final RegistryObject<Item> MARULA = ITEMS.register("marula",
