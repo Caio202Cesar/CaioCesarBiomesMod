c818b54f69ea8dd2b5b058c4307f3f3dc3f45cd6
ADM Caio <caiodnobre@gmail.com>
2025-08-23 21:59:13 -0300

    Added pistachio planks and pine planks recipe from pinyon wood.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/block/ModWood.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/ModWood.java
@@ -138,6 +138,8 @@ public class ModWood {
             ModLogs::new);
     public static final RegistryObject<Block> PLANE_PLANKS = registerBlock("plane_planks",
             ModLogs::new);
+    public static final RegistryObject<Block> PISTACHIO_PLANKS = registerBlock("pistachio_planks",
+            ModLogs::new);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
