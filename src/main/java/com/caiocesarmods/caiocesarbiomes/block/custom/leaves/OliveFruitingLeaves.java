b110f87ed915994e78c41bc58f698dcdf7eb5d36
ADM Caio <caiodnobre@gmail.com>
2025-09-28 21:54:36 -0300

    Fruiting olive leaves drop 3 olives por leave block.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/leaves/OliveFruitingLeaves.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/leaves/OliveFruitingLeaves.java
@@ -47,7 +47,7 @@ public class OliveFruitingLeaves extends LeavesBlock implements IForgeShearable 
 
         if ("WINTER".equals(currentSeason) && nextStage != null && random.nextInt(60) == 0) {
 
-            int dropCount = 1;
+            int dropCount = 3;
 
             ItemStack itemStack = new ItemStack(ModItems.OLIVES.get(), dropCount);
             ItemEntity itemEntity = new ItemEntity(worldIn, pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5, itemStack);
@@ -67,7 +67,7 @@ public class OliveFruitingLeaves extends LeavesBlock implements IForgeShearable 
     public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
         if (!worldIn.isRemote) {
 
-            int dropCount = 1;
+            int dropCount = 3;
 
             ItemStack itemStack = new ItemStack(ModItems.OLIVES.get(), dropCount);
             ItemEntity itemEntity = new ItemEntity(worldIn, pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5, itemStack);
