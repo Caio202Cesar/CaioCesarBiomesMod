1fd4b85fad2715b18770b55e300b8ca91a5e4c7c
ADM Caio <caiodnobre@gmail.com>
2025-12-10 20:27:57 -0300

    Tropical seasonal forest is now a biome with no rain, since it is based on tropical dry forests, with a pronunced dry and wet seasons (like the savannas). Plus, its color is now less saturated than the jungle edge's and more green than the savanna's, estabilishing it as a transition biome.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/TropicalSeasonalForestBiome.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/TropicalSeasonalForestBiome.java
@@ -61,10 +61,10 @@ public class TropicalSeasonalForestBiome {
 
       biomegenerationsettings$builder.withFeature(GenerationStage.Decoration.LAKES, Features.LAKE_LAVA);
 
-      return (new Biome.Builder()).precipitation(Biome.RainType.RAIN).category(Biome.Category.SAVANNA).depth(depth).scale(scale)
-              .temperature(1.0F).downfall(0.69F).setEffects((new BiomeAmbience.Builder()).setWaterColor(4159204)
-                      .setWaterFogColor(4159204).withSkyColor(7842047).withFoliageColor(4110351)
-                      .withGrassColor(5877296).setFogColor(7907327)
+      return (new Biome.Builder()).precipitation(Biome.RainType.NONE).category(Biome.Category.SAVANNA).depth(depth).scale(scale)
+              .temperature(1.0F).downfall(0.5F).setEffects((new BiomeAmbience.Builder()).setWaterColor(4159204)
+                      .setWaterFogColor(4159204).withSkyColor(7842047).withFoliageColor(6862863)
+                      .withGrassColor(8832831).setFogColor(7907327)
                       .setAmbientSound(SoundEvents.MUSIC_CREATIVE)
                       .setMusic(BackgroundMusicTracks.getDefaultBackgroundMusicSelector(SoundEvents.MUSIC_CREATIVE))
                       .build())
