f5184633d072405326351bdd3aac2e60e97aae36
ADM Caio <caiodnobre@gmail.com>
2025-12-22 16:59:52 -0300

    Added new biome: Ginkgo Forest

--- src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/leaves/FremontPoplarWinterBranches.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/leaves/FremontPoplarWinterBranches.java
@@ -38,7 +38,7 @@ public class FremontPoplarWinterBranches extends LeavesBlock implements IForgeSh
     public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
         String currentSeason = Season.getSeason(worldIn.getDayTime());
 
-        if ("SPRING".equals(currentSeason) && nextStage != null && random.nextInt(25) == 0) {
+        if ("SPRING".equals(currentSeason) && nextStage != null && random.nextInt(15) == 0) {
 
             int distance = state.get(LeavesBlock.DISTANCE);
             boolean persistent = state.get(LeavesBlock.PERSISTENT);
@@ -47,6 +47,16 @@ public class FremontPoplarWinterBranches extends LeavesBlock implements IForgeSh
 
             worldIn.setBlockState(pos, newState, 2);
 
+        }
+
+        if ("SUMMER".equals(currentSeason) && nextStage != null && random.nextInt(5) == 0) {
+
+            int distance = state.get(LeavesBlock.DISTANCE);
+            boolean persistent = state.get(LeavesBlock.PERSISTENT);
+
+            BlockState newState = nextStage.get().getDefaultState().with(LeavesBlock.DISTANCE, distance).with(LeavesBlock.PERSISTENT, persistent);
+
+            worldIn.setBlockState(pos, newState, 2);
         }
     }
 
