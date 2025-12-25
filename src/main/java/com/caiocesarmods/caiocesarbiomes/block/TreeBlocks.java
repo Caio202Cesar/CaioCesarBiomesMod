f3e4e3b11aff1ded2606bf072bc675587c163414
ADM Caio <caiodnobre@gmail.com>
2025-12-25 01:18:31 -0300

    Added a command to set season.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/block/TreeBlocks.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/TreeBlocks.java
@@ -1943,7 +1943,7 @@ public class TreeBlocks {
 
     public static final RegistryObject<Block> WALNUT_LEAVES = registerBlock("walnut_leaves",
             () -> new WalnutLeaves(AbstractBlock.Properties.create(Material.LEAVES).hardnessAndResistance(0.2F).tickRandomly()
-                    .notSolid().sound(SoundType.PLANT).harvestTool(ToolType.HOE), TreeBlocks.WALNUT_FRUITING_LEAVES));
+                    .notSolid().sound(SoundType.PLANT).harvestTool(ToolType.HOE)));
     public static final RegistryObject<Block> WALNUT_FRUITING_LEAVES = registerBlock("walnut_fruiting_leaves",
             () -> new WalnutFruitingLeaves(AbstractBlock.Properties.create(Material.LEAVES).hardnessAndResistance(0.2F).tickRandomly()
                     .notSolid().sound(SoundType.PLANT).harvestTool(ToolType.HOE), TreeBlocks.WALNUT_FALL_LEAVES));
