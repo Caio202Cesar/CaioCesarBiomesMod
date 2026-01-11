f99d038f0a1b42581da89aa7209536fd665ee245
ADM Caio <caiodnobre@gmail.com>
2026-01-11 20:27:03 -0300

    Added almond tree to hemiboreal steepe biome.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/Saplings/WhiteOleanderSapling.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/Saplings/WhiteOleanderSapling.java
@@ -6,11 +6,19 @@ import net.minecraft.block.*;
 import net.minecraft.block.trees.Tree;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -18,25 +26,94 @@ import javax.annotation.Nullable;
 import java.util.Random;
 
 public class WhiteOleanderSapling extends SaplingBlock {
-        public WhiteOleanderSapling() {
-            super(new WhiteOleanderShrub(), AbstractBlock.Properties.from(Blocks.OAK_SAPLING).hardnessAndResistance(0.0f)
+    public WhiteOleanderSapling() {
+        super(new WhiteOleanderShrub(), AbstractBlock.Properties.from(Blocks.OAK_SAPLING).hardnessAndResistance(0.0f)
                     .sound(SoundType.PLANT));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void registerRenderLayer() {
+        RenderTypeLookup.setRenderLayer(TreeBlocks.OLEANDER_WHITE_SAPLING.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(TreeBlocks.POTTED_OLEANDER_WHITE_SAPLING.get(), RenderType.getCutout());
+
+    }
+
+    //Hardy to zone 8
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        float biomeTemp = world.getBiome(pos).getTemperature(pos);
+        float minTemp = 0.75f;
+        float maxTemp = 2.00f;
+
+        if (biomeTemp >= minTemp && biomeTemp <= maxTemp) {
+            // Only attempt natural growth in suitable biomes
+            super.randomTick(state, world, pos, random);
+        }
+        // If biome temperature is too low/high, do nothing (block natural growth)
+    }
+
+    @Override
+    public boolean canGrow(IBlockReader worldIn, BlockPos pos, BlockState state, boolean isClient) {
+        if (!(worldIn instanceof World)) {
+            return false;
         }
 
-        @OnlyIn(Dist.CLIENT)
-        public static void registerRenderLayer() {
-            RenderTypeLookup.setRenderLayer(TreeBlocks.OLEANDER_WHITE_SAPLING.get(), RenderType.getCutout());
-            RenderTypeLookup.setRenderLayer(TreeBlocks.POTTED_OLEANDER_WHITE_SAPLING.get(), RenderType.getCutout());
+        World world = (World) worldIn;
 
+        Biome biome = world.getBiome(pos);
+        float temp = biome.getTemperature(pos);
+
+        // ---- YOUR TEMPERATURE RESTRICTION LOGIC ----
+        boolean tooHot = temp > 2.00F;
+        boolean tooCold = temp < 0.75F;
+
+        if (tooHot || tooCold) {
+            return false;
         }
 
-        public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
-            return 80;
-        }
+        return super.canGrow(worldIn, pos, state, isClient);
+    }
 
-        public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
-            return 60;
+    @Override
+    public boolean canUseBonemeal(World worldIn, Random random, BlockPos pos, BlockState state) {
+        // Always allow for the check, we'll block in grow()
+        return true;
+    }
+
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+        if (!worldIn.isRemote) {
+            float temp = worldIn.getBiome(pos).getTemperature(pos);
+            float minTemp = 0.75f, maxTemp = 2.00f;
+
+            if (temp < minTemp) {
+                player.sendMessage(
+                        new StringTextComponent("This biome is too cold for this sapling."),
+                        player.getUniqueID()
+                );
+                return ActionResultType.SUCCESS; // Prevent further processing if needed
+            }
+
+            if (temp > maxTemp) {
+                player.sendMessage(
+                        new StringTextComponent("This biome is too hot for this sapling."),
+                        player.getUniqueID()
+                );
+                return ActionResultType.SUCCESS; // Prevent further processing if needed
+            }
+
+            // If temp is in range, optionally allow normal processing:
+            // return super.onBlockActivated(...);
+            return super.onBlockActivated(state, worldIn, pos, player, handIn, hit);
         }
+        return ActionResultType.SUCCESS;
+    }
+
+    public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+            return 80;}
+
+    public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+            return 60;}
 
     private static class WhiteOleanderShrub extends Tree {
         @Nullable
