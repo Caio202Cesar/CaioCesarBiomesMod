a87945ef43cdd49b6d05bdf40a764753b78a2e25
ADM Caio <caiodnobre@gmail.com>
2026-01-21 00:47:22 -0300

    Removed spanish translation.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/HardinessZones.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/HardinessZones.java
@@ -84,6 +84,7 @@ public class HardinessZones {
         SAPLING_ZONES.put(new ResourceLocation("brbiomesmod", "brazilnut_sapling"), "min 12");
         SAPLING_ZONES.put(new ResourceLocation("brbiomesmod", "brazilwood_sapling"), "min 10");
         SAPLING_ZONES.put(new ResourceLocation("brbiomesmod", "brazillian_sassafras_sapling"), "min 9");
+        SAPLING_ZONES.put(new ResourceLocation("brbiomesmod", "buriti_sapling"), "min 11");
 
         SAPLING_ZONES.put(new ResourceLocation("brbiomesmod", "jabuticaba_sapling"), "min 9");
         SAPLING_ZONES.put(new ResourceLocation("brbiomesmod", "sapucaia_sapling"), "min 10");
