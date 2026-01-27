b8719a04843876da4e322c20eac3b08ed7e51781
ADM Caio <caiodnobre@gmail.com>
2026-01-27 06:16:38 -0300

    Added plumeria trees.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/Climate/ClimateSetup.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/Climate/ClimateSetup.java
@@ -1,7 +1,5 @@
 package com.caiocesarmods.caiocesarbiomes.Climate;
 
-import com.caiocesarmods.caiocesarbiomes.World.worldgen.Biomes.BambooSubtropicalLaurelJungleBiome;
-import net.minecraft.world.biome.Biomes;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -12,15 +10,27 @@ public class ClimateSetup {
     @SubscribeEvent
     public static void setup(final FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
-            SummerHeatRegistry.register("minecraft:desert", SummerHeat.HOT);
+            SummerHeatRegistry.register("minecraft:desert", SummerHeat.SCHORCHING);
             SummerHeatRegistry.register("minecraft:savanna", SummerHeat.HOT);
             SummerHeatRegistry.register("minecraft:jungle", SummerHeat.HOT);
-
             SummerHeatRegistry.register("minecraft:plains", SummerHeat.HOT);
             SummerHeatRegistry.register("minecraft:forest", SummerHeat.HOT);
-
             SummerHeatRegistry.register("minecraft:taiga", SummerHeat.COOLER);
             SummerHeatRegistry.register("minecraft:snowy_tundra", SummerHeat.COLD);
+
+            SummerHeatRegistry.register("caiocesarbiomes:bamboo_subtropical_laurel_jungle", SummerHeat.HOT);
+            SummerHeatRegistry.register("caiocesarbiomes:bunya_subtropical_laurel_forest", SummerHeat.HOT);
+
+            SummerHeatRegistry.register("caiocesarbiomes:coastal_subtropical_desert", SummerHeat.WARM);
+            SummerHeatRegistry.register("caiocesarbiomes:coastal_subtropical_desert_lomas", SummerHeat.WARM);
+            SummerHeatRegistry.register("caiocesarbiomes:tropical_pine_island", SummerHeat.HOT);
+            SummerHeatRegistry.register("caiocesarbiomes:cork_oak_savanna", SummerHeat.HOT);
+            SummerHeatRegistry.register("caiocesarbiomes:dry_tropical_beach", SummerHeat.HOT);
+            SummerHeatRegistry.register("caiocesarbiomes:eucalyptus_plains", SummerHeat.HOT);
+            SummerHeatRegistry.register("caiocesarbiomes:eucalyptus_sahel", SummerHeat.SCHORCHING);
+            SummerHeatRegistry.register("caiocesarbiomes:eucalyptus_savanna", SummerHeat.HOT);
+            SummerHeatRegistry.register("caiocesarbiomes:ginkgo_forest", SummerHeat.HOT);
+
         });
     }
 }
