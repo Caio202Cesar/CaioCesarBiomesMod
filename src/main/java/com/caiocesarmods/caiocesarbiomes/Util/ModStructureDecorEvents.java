ff45399bc3e50365f63e2479ce4fd172a7e69ca5
ADM Caio <caiodnobre@gmail.com>
2026-02-01 14:35:11 -0300

    Swamp huts have a chance of spawning oleander or yellow oleander saplings instead mushroom.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/Util/ModStructureDecorEvents.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/Util/ModStructureDecorEvents.java
@@ -6,6 +6,8 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.feature.structure.StructureStart;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -24,10 +26,10 @@ public class ModStructureDecorEvents {
         Chunk chunk = (Chunk) event.getChunk();
 
         // Check structures in this chunk
-        if (!world.getChunkProvider().getChunkGenerator()
-                .hasStructure(world, Structure.WITCH_HUT, chunk.getPos())) {
-            return;
-        }
+        StructureStart<?> start =
+                chunk.getStructureStarts().get(Structure.SWAMP_HUT);
+
+        if (start == StructureStart.DUMMY) return;
 
         BlockPos.Mutable pos = new BlockPos.Mutable();
         Random rand = world.rand;
@@ -51,10 +53,50 @@ public class ModStructureDecorEvents {
                             state.getBlock() == Blocks.POTTED_BROWN_MUSHROOM) {
 
                         // 🎲 Chance-based replacement
-                        if (rand.nextFloat() < 0.30f) { // 30% chance
+                        if (rand.nextFloat() < 0.15f) { // 15% chance = rarest
                             world.setBlockState(
                                     pos,
                                     TreeBlocks.POTTED_OLEANDER_WHITE_SAPLING.get().getDefaultState(),
+                                    2
+                            );
+                        }
+
+                        if (rand.nextFloat() < 0.55f) { // 55% chance (most common)
+                            world.setBlockState(
+                                    pos,
+                                    TreeBlocks.POTTED_OLEANDER_PINK_SAPLING.get().getDefaultState(),
+                                    2
+                            );
+                        }
+
+                        if (rand.nextFloat() < 0.45f) { // 45% chance - third common
+                            world.setBlockState(
+                                    pos,
+                                    TreeBlocks.POTTED_OLEANDER_DARK_PINK_SAPLING.get().getDefaultState(),
+                                    2
+                            );
+                        }
+
+                        if (rand.nextFloat() < 0.25f) { // 20% chance = second rarer
+                            world.setBlockState(
+                                    pos,
+                                    TreeBlocks.POTTED_OLEANDER_RED_SAPLING.get().getDefaultState(),
+                                    2
+                            );
+                        }
+
+                        if (rand.nextFloat() < 0.20f) { // 20% chance = third rarest
+                            world.setBlockState(
+                                    pos,
+                                    TreeBlocks.POTTED_ORANGE_OLEANDER_SAPLING.get().getDefaultState(),
+                                    2
+                            );
+                        }
+
+                        if (rand.nextFloat() < 0.50f) { // 50% chance = second commonest
+                            world.setBlockState(
+                                    pos,
+                                    TreeBlocks.POTTED_YELLOW_OLEANDER_SAPLING.get().getDefaultState(),
                                     2
                             );
                         }
