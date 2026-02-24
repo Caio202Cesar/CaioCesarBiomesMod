47c5db99c0b77c41281b63d67ba32fb7bbc80293
ADM Caio <caiodnobre@gmail.com>
2026-02-24 07:21:39 -0300

    Fixed Pewen Coniferous Grove.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/leaves/PohutukawaLeaves.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/leaves/PohutukawaLeaves.java
@@ -1,26 +1,23 @@
 package com.caiocesarmods.caiocesarbiomes.block.custom.leaves;
 
 import com.caiocesarmods.caiocesarbiomes.Seasons.Season;
-import net.minecraft.block.Block;
+import com.caiocesarmods.caiocesarbiomes.block.TreeBlocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LeavesBlock;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.IForgeShearable;
 
 import java.util.Random;
-import java.util.function.Supplier;
 
 public class PohutukawaLeaves extends LeavesBlock implements IForgeShearable {
-    private final Supplier<Block> nextStage;
-
-    public PohutukawaLeaves(Properties properties, Supplier<Block> nextStage) {
+    public PohutukawaLeaves(Properties properties) {
         super(properties);
-        this.nextStage = nextStage;
-    }
 
+    }
 
     public boolean ticksRandomly(BlockState state) {
         return true;
@@ -38,18 +35,28 @@ public class PohutukawaLeaves extends LeavesBlock implements IForgeShearable {
     public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
         String currentSeason = Season.getSeason(worldIn.getDayTime());
 
-        if ("SUMMER".equals(currentSeason) && nextStage != null && random.nextInt(15) == 0) {
+        Biome biome = worldIn.getBiome(pos);
+        float temp = biome.getTemperature(pos);
 
+        //Pattern for tropical biomes
+        if (temp >= 0.9F && random.nextInt(75) == 0) {
             int distance = state.get(LeavesBlock.DISTANCE);
             boolean persistent = state.get(LeavesBlock.PERSISTENT);
 
-            BlockState newState = nextStage.get().getDefaultState().with(LeavesBlock.DISTANCE, distance).with(LeavesBlock.PERSISTENT, persistent);
+            worldIn.setBlockState(pos, TreeBlocks.POHUTUKAWA_SPARSE_FLOWERING_LEAVES.get()
+                    .getDefaultState().with(LeavesBlock.DISTANCE, distance).with(LeavesBlock.PERSISTENT, persistent), 3);
+            return;
+        }
 
-            worldIn.setBlockState(pos, newState, 2);
+        //Pattern for subtropical biomes
+        if (temp <= 0.89F && "SUMMER".equals(currentSeason) && random.nextInt(75) == 0) {
+            int distance = state.get(LeavesBlock.DISTANCE);
+            boolean persistent = state.get(LeavesBlock.PERSISTENT);
 
+            worldIn.setBlockState(pos, TreeBlocks.POHUTUKAWA_FLOWERING_LEAVES.get()
+                    .getDefaultState().with(LeavesBlock.DISTANCE, distance).with(LeavesBlock.PERSISTENT, persistent), 3);
         }
     }
-
 
     public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
         return 90;
