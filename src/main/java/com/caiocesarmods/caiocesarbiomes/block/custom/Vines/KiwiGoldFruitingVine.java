d18e04f0ce493efa22d81912424256438db781d4
ADM Caio <caiodnobre@gmail.com>
2026-03-24 23:49:54 -0300

    Tropical Almond leaves drop fruit during spring in tropical climates and during summer in subtropical climates.

---
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/custom/Vines/KiwiGoldFruitingVine.java
@@ -0,0 +1,137 @@
+package com.caiocesarmods.caiocesarbiomes.block.custom.Vines;
+
+import com.caiocesarmods.caiocesarbiomes.Seasons.Season;
+import com.caiocesarmods.caiocesarbiomes.block.ModPlants;
+import com.caiocesarmods.caiocesarbiomes.item.ModItems;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.VineBlock;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.IForgeShearable;
+import net.minecraftforge.common.PlantType;
+import net.minecraftforge.common.ToolType;
+
+import java.util.Random;
+
+public class KiwiGoldFruitingVine extends VineBlock implements IForgeShearable {
+    public KiwiGoldFruitingVine() {
+        super(Properties.from(Blocks.VINE).tickRandomly().doesNotBlockMovement().notSolid()
+                .zeroHardnessAndResistance().sound(SoundType.VINE).harvestTool(ToolType.HOE));
+    }
+
+    public boolean ticksRandomly(BlockState state) {
+        return true;
+    }
+
+    /**
+     * Performs a random tick on a block.
+     *
+     * @param state
+     * @param worldIn
+     * @param pos
+     * @param random
+     */
+    public static final float MIN_TEMP = 0.75F;
+    public static final float MAX_TEMP = 0.89F;
+
+    //Hardy from zone 8 to 9
+    @Override
+    public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
+        String currentSeason = Season.getSeason(worldIn.getDayTime());
+
+        Biome biome = worldIn.getBiome(pos);
+        float temp = biome.getTemperature(pos);
+
+        if ("WINTER".equals(currentSeason) && random.nextInt(2) == 0) {
+
+            int dropCount = 1;
+
+            ItemStack itemStack = new ItemStack(ModItems.GOLD_KIWI_FRUITS.get(), dropCount);
+            ItemEntity itemEntity = new ItemEntity(worldIn, pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5, itemStack);
+
+            worldIn.addEntity(itemEntity);
+
+            BlockState currentState = state;
+            BlockState newState = ModPlants.KIWI_GOLD_WINTER_DRY_VINE.get().getDefaultState();
+
+            worldIn.setBlockState(pos, ModPlants.KIWI_GOLD_WINTER_DRY_VINE.get().getDefaultState());
+
+            newState = newState.with(VineBlock.NORTH, currentState.get(VineBlock.NORTH)).with(VineBlock.EAST, currentState.get(VineBlock.EAST))
+                    .with(VineBlock.SOUTH, currentState.get(VineBlock.SOUTH)).with(VineBlock.WEST, currentState.get(VineBlock.WEST));
+
+            worldIn.setBlockState(pos, newState, 3);
+        }
+
+        if ("SPRING".equals(currentSeason) && random.nextInt(2) == 0) {
+
+            int dropCount = 1;
+
+            ItemStack itemStack = new ItemStack(ModItems.KIWI_GOLD_FRUITS.get(), dropCount);
+            ItemEntity itemEntity = new ItemEntity(worldIn, pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5, itemStack);
+
+            worldIn.addEntity(itemEntity);
+
+            BlockState currentState = state;
+            BlockState newState = ModPlants.KIWI_GOLD_VINE.get().getDefaultState();
+
+            worldIn.setBlockState(pos, ModPlants.KIWI_GOLD_VINE.get().getDefaultState());
+
+            newState = newState.with(VineBlock.NORTH, currentState.get(VineBlock.NORTH)).with(VineBlock.EAST, currentState.get(VineBlock.EAST))
+                    .with(VineBlock.SOUTH, currentState.get(VineBlock.SOUTH)).with(VineBlock.WEST, currentState.get(VineBlock.WEST));
+
+            worldIn.setBlockState(pos, newState, 3);
+        }
+
+        //Dies outside its hardiness zone
+        if (temp < MIN_TEMP || temp > MAX_TEMP) {
+            worldIn.destroyBlock(pos, false);
+        }
+    }
+
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+        if (!worldIn.isRemote) {
+
+            ItemStack itemStack = new ItemStack(ModItems.KIWI_GOLD_FRUITS.get());
+            ItemEntity itemEntity = new ItemEntity(worldIn, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, itemStack);
+
+            worldIn.addEntity(itemEntity);
+
+            BlockState currentState = state;
+            BlockState newState = ModPlants.KIWI_GOLD_VINE.get().getDefaultState();
+
+            newState = newState.with(VineBlock.NORTH, currentState.get(VineBlock.NORTH)).with(VineBlock.EAST, currentState.get(VineBlock.EAST))
+                    .with(VineBlock.SOUTH, currentState.get(VineBlock.SOUTH)).with(VineBlock.WEST, currentState.get(VineBlock.WEST));
+
+            worldIn.setBlockState(pos, newState, 3);
+
+            worldIn.playSound(null, pos, SoundEvents.BLOCK_GRASS_BREAK, SoundCategory.BLOCKS, 1.0F, 1.0F);
+
+        }
+        return ActionResultType.SUCCESS;
+    }
+
+    public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+        return 90;
+    }
+
+    public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+        return 85;
+    }
+
+    public PlantType getPlantType(IBlockReader world, BlockPos pos) {
+        return PlantType.CROP;
+    }
+}
+
+
