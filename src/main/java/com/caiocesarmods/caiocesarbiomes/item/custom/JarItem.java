ca00373a50be57632730088f848cccdd3661a6ab
Caio202Cesar <caiodnobre@gmail.com>
2026-07-05 06:25:31 -0300

    Added ivy to ocotea trees.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/item/custom/JarItem.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/item/custom/JarItem.java
@@ -4,6 +4,8 @@ import com.caiocesarmods.caiocesarbiomes.item.ModItems;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
+import java.util.Random;
+
 public class JarItem extends Item {
     public JarItem(Properties properties) {
         super(properties);
@@ -15,8 +17,16 @@ public class JarItem extends Item {
     }
 
     @Override
-    public ItemStack getContainerItem(ItemStack itemStack) {
-        Item containerItem = ModItems.JAR.get();
-        return new ItemStack(containerItem);
+    public ItemStack getContainerItem(ItemStack stack) {
+        ItemStack container = stack.copy();
+
+        Random rand = new Random();
+
+        if (container.attemptDamageItem(1, rand, null)) {
+            // Fully consumed: return the empty jar
+            return new ItemStack(ModItems.JAR.get());
+        }
+
+        return container;
     }
 }
