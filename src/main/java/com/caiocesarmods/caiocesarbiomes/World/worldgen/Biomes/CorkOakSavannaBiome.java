c08b60b6bcd53538cbb6cac04965567fe8912167
Caio202Cesar <caiodnobre@gmail.com>
2026-07-18 20:28:30 -0300

    Changed Oleander gardens hardiness zone to 10 again, and shifted mediterranean pine forest to zone 9, as well as cork oak savanna.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/CorkOakSavannaBiome.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/CorkOakSavannaBiome.java
@@ -62,9 +62,9 @@ public class CorkOakSavannaBiome {
         ModDefaultBiomeFeatures.withMediterraneanShrubs(biomegenerationsettings$builder);
         ModDefaultBiomeFeatures.withMediterraneanSavannaSmallTrees(biomegenerationsettings$builder);
 
-        //Hardiness zone 10: 0.85F - 0.89F
+        //Hardiness zone 9: 0.8F - 0.84F
         return (new Biome.Builder()).precipitation(Biome.RainType.NONE).category(Biome.Category.PLAINS).depth(depth).scale(scale)
-                .temperature(0.89F).downfall(0.2F).setEffects((new BiomeAmbience.Builder()).setWaterColor(4159204)
+                .temperature(0.84F).downfall(0.2F).setEffects((new BiomeAmbience.Builder()).setWaterColor(4159204)
                         .setWaterFogColor(4159204).withSkyColor(8103167).withFoliageColor(7441937)
                         .withGrassColor(12564309).setFogColor(13494015)
                         .setAmbientSound(ModSoundEvents.MEDITERRANEAN_AMBIENCE.get())
