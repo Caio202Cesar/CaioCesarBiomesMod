8a93895b8aa4c11db004f41487a85a08f691817c
Caio202Cesar <caiodnobre@gmail.com>
2026-07-18 02:06:59 -0300

    Change coast cottonwood small tree shape from blob to forky.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/Util/Layers/ModEdgeLayer.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Biomes/Util/Layers/ModEdgeLayer.java
@@ -1,58 +1,61 @@
 package com.caiocesarmods.caiocesarbiomes.World.worldgen.Biomes.Util.Layers;
 
 import com.caiocesarmods.caiocesarbiomes.Api.ModBiomes;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.INoiseRandom;
 import net.minecraft.world.gen.layer.traits.ICastleTransformer;
 import net.minecraft.world.gen.layer.LayerUtil;
-
-import java.util.Optional;
+import net.minecraftforge.fml.RegistryObject;
 
 public enum ModEdgeLayer implements ICastleTransformer {
 
     INSTANCE;
 
     @Override
-    public int apply(INoiseRandom context, int north, int west, int south, int east, int center) {
-        int[] outBiomeId = new int[1];
-        
-        if (this.replaceBiomeEdge(outBiomeId, north, east, south, west, center, ModBiomes.mediterranean_oak_woodland,
-                ModBiomes.mediterranean_oak_sparse_woodland)) { return outBiomeId[0];
+    public int apply(INoiseRandom random,
+                     int north,
+                     int west,
+                     int south,
+                     int east,
+                     int center) {
+
+        // Mediterranean Oak Woodland -> Sparse Woodland edge
+        if (replaceEdge(
+                north, west, south, east, center,
+                ModBiomes.mediterranean_oak_woodland,
+                ModBiomes.mediterranean_oak_sparse_woodland)) {
+
+            return biomeId(ModBiomes.mediterranean_oak_sparse_woodland);
         }
 
         return center;
     }
 
-    private boolean replaceBiomeEdge(int[] outId, int northBiomeId, int eastBiomeId, int southBiomeId, int westBiomeId, int biomeId,
-                                     Optional<Biome> fromBiome, Optional<Biome> toBiome) {
+    private boolean replaceEdge(
+            int north,
+            int west,
+            int south,
+            int east,
+            int center,
+            java.util.Optional<Biome> base,
+            java.util.Optional<Biome> edge) {
 
-        return fromBiome.isPresent() && toBiome.isPresent() && this.replaceBiomeEdge(outId, northBiomeId, eastBiomeId, southBiomeId, westBiomeId, biomeId,
-                WorldGenRegistries.BIOME.getId(fromBiome.get()), WorldGenRegistries.BIOME.getId(toBiome.get()));
-
-    }
-
-    private boolean replaceBiomeEdge(int[] outId, int northBiomeId, int eastBiomeId, int southBiomeId, int westBiomeId, int biomeId,
-                                     Optional<Biome> fromBiome, int toBiome) {
-
-        return fromBiome.isPresent() && this.replaceBiomeEdge(outId, northBiomeId, eastBiomeId, southBiomeId, westBiomeId, biomeId,
-                WorldGenRegistries.BIOME.getId(fromBiome.get()), toBiome);
-    }
-
-    private boolean replaceBiomeEdge(int[] outId, int northBiomeId, int eastBiomeId, int southBiomeId, int westBiomeId,
-                                     int biomeId, int fromBiome, int toBiome) {
-        if (biomeId != fromBiome) {
+        if (!base.isPresent() || !edge.isPresent())
             return false;
-        } else {
-            if (LayerUtil.areBiomesSimilar(northBiomeId, fromBiome) && LayerUtil.areBiomesSimilar(eastBiomeId, fromBiome)
-                    && LayerUtil.areBiomesSimilar(westBiomeId, fromBiome) && LayerUtil.areBiomesSimilar(southBiomeId, fromBiome)) {
-                outId[0] = biomeId;
-            } else {
-                outId[0] = toBiome;
-            }
 
-            return true;
-        }
+        int baseId = WorldGenRegistries.BIOME.getId(base.get());
+
+        if (center != baseId)
+            return false;
+
+        return !(LayerUtil.areBiomesSimilar(north, baseId)
+                && LayerUtil.areBiomesSimilar(south, baseId)
+                && LayerUtil.areBiomesSimilar(east, baseId)
+                && LayerUtil.areBiomesSimilar(west, baseId));
+    }
+
+    private int biomeId(java.util.Optional<Biome> biome) {
+        return WorldGenRegistries.BIOME.getId(biome.get());
     }
 }
