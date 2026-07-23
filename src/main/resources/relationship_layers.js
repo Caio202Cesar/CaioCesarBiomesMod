f86be4879803fd75ea28bb6f071676074e686990
Caio202Cesar <caiodnobre@gmail.com>
2026-07-22 22:24:54 -0300

    Removed Canary Pine from the Subtropical Pine Dry Forest, now Mediterranean Pine Forest. It is now included in the Laurel Forest biomes (less frequent in Laurel Forest and Laurel Jungle) and more frequente in the Highland sub-biome.

--- src/main/resources/relationship_layers.js
+++ src/main/resources/relationship_layers.js
@@ -1,3 +1,32 @@
 function initializeCoreMod() {
-    return {};
+    return {
+        "RelationshipLayers": {
+            target: {
+                type: "METHOD",
+                class: "net.minecraft.world.gen.layer.LayerUtil",
+                methodName: "setupOverworldLayer",
+                methodDesc: "(ZIILjava/util/function/LongFunction;)Lnet/minecraft/world/gen/area/IAreaFactory;"
+            },
+            transformer: function(method) {
+
+                var instructions = method.instructions;
+
+                for (var insn = instructions.getFirst();
+                     insn != null;
+                     insn = insn.getNext()) {
+
+                    if (insn.getOpcode() == Opcodes.GETSTATIC
+                        && insn.owner == "net/minecraft/world/gen/layer/AddBambooForestLayer"
+                        && insn.name == "INSTANCE") {
+
+                        print("[CaioCesarBiomes] Found AddBambooForestLayer!");
+
+                        break;
+                    }
+                }
+
+                return method;
+            }
+        }
+    };
 }
