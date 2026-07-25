91caf2daa5cfde4f9ec1356ed8d427c1412be354
Caio202Cesar <caiodnobre@gmail.com>
2026-07-25 01:26:33 -0300

    Stone pine cannot grow in zone 11 biomes anymore.

---
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/entity/EntityEvents.java
@@ -0,0 +1,25 @@
+package com.caiocesarmods.caiocesarbiomes.entity;
+
+import com.caiocesarmods.caiocesarbiomes.CaioCesarBiomesMod;
+import com.caiocesarmods.caiocesarbiomes.entity.goals.AvoidCamphorGoal;
+import net.minecraft.entity.monster.CreeperEntity;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = CaioCesarBiomesMod.MOD_ID)
+public class EntityEvents {
+    @SubscribeEvent
+    public static void onEntityJoin(EntityJoinWorldEvent event) {
+
+        if (!(event.getEntity() instanceof CreeperEntity))
+            return;
+
+        CreeperEntity creeper = (CreeperEntity) event.getEntity();
+
+        creeper.goalSelector.addGoal(
+                1,
+                new AvoidCamphorGoal(creeper, 1.0D, 1.25D)
+        );
+    }
+}
