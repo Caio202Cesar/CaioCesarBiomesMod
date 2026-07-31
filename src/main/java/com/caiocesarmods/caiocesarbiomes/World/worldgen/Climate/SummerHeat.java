f6cdc0e89d63692d340493fb0057e3688d50f4e8
Caio202Cesar <caiodnobre@gmail.com>
2026-07-30 22:05:28 -0300

    Removed birch trees from Ginkgo Forest biome, as its summer temperature was set to hot.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Climate/SummerHeat.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Climate/SummerHeat.java
@@ -2,14 +2,14 @@ package com.caiocesarmods.caiocesarbiomes.World.worldgen.Climate;
 
 public enum SummerHeat {
 
-    ABSENT(Float.NEGATIVE_INFINITY, -0.11F),
-    ICY( -0.10F, 0.14F),
-    TUNDRA_COLD(0.15F, 0.29F),
-    COOLER(0.3F, 0.49F),
-    MILD(0.5F, 0.69F),
-    WARM(0.7F, 0.79F),
-    HOT(0.8F, 0.89F),
-    VERY_HOT(0.9F, 0.99F),
+    ABSENT(Float.NEGATIVE_INFINITY, -0.10F),
+    ICY( -0.10F, 0.15F),
+    TUNDRA_COLD(0.15F, 0.3F),
+    COOLER(0.3F, 0.5F),
+    MILD(0.5F, 0.7F),
+    WARM(0.7F, 0.8F),
+    HOT(0.8F, 0.9F),
+    VERY_HOT(0.9F, 1.0F),
     SCORCHING(1.0F, Float.POSITIVE_INFINITY);
 
     private final float min;
