e13bced16870cd375164ef2e7570777aebbb36e6
Caio202Cesar <caiodnobre@gmail.com>
2026-07-30 21:57:16 -0300

    Removed birch trees from Ginkgo Forest biome, as its summer temperature was set to hot.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Climate/SummerHeatHelper.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/World/worldgen/Climate/SummerHeatHelper.java
@@ -2,15 +2,10 @@ package com.caiocesarmods.caiocesarbiomes.World.worldgen.Climate;
 
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.biome.Biome;
 
 public class SummerHeatHelper {
 
     public static float get(World world, BlockPos pos) {
-
-        Biome biome = world.getBiome(pos);
-
-        float summer = SummerTemperatureRegistry.get(world, pos);
 
         float altitudeModifier = (pos.getY() - 64) * -0.005F;
 
