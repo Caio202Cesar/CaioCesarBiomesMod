413b05e58d485d4d269e00e4b43397e9e1f96738
Caio202Cesar <caiodnobre@gmail.com>
2026-08-06 14:02:26 -0300

    Removed avocado and ocotea wood. Both will be replaced by Laurel Wood.

--- src/main/java/com/caiocesarmods/caiocesarbiomes/block/ModWood.java
+++ src/main/java/com/caiocesarmods/caiocesarbiomes/block/ModWood.java
@@ -164,6 +164,8 @@ public class ModWood {
             ModLogs::new);
     public static final RegistryObject<Block> LAUREL_PLANKS = registerBlock("laurel_planks",
             ModLogs::new);
+    public static final RegistryObject<Block> PODOCARPUS_PLANKS = registerBlock("podocarpus_planks",
+            ModLogs::new);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
